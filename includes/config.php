0384f8813a71d88255b0789270cd8da7421168de
lawfyd <dimonst93333@gmail.com>
2018-09-03 17:56:25 +0300

    Initial commit

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+$cofig = array(
+	'title' => 'Blog of Dima',
+	'db' => array(
+			'server' => 'localhost',
+			'username' => 'root',
+			'password' => '',
+			'name' => 'test_blog'
+	)
+
+);
