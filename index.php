628a160eae967e1b0e1bab8cdfd3a75f79b882a9
lawfyd <dimonst93333@gmail.com>
2018-09-04 14:53:07 +0300

    output new articles, and categories

--- index.php
+++ index.php
@@ -55,9 +55,9 @@ require "includes/config.php";
                             }
                           }
                         ?>
-                        <small>Категория: <a href="/articles.php?id=<?php echo $art_cat['id'] ?>"><?php echo $art_cat['title']; ?></a></small>
+                        <small>Категория: <a href="/articles.php?categorie=<?php echo $art_cat['id'] ?>"><?php echo $art_cat['title']; ?></a></small>
                       </div>
-                      <div class="article__info__preview"><?php echo mb_substr($art['text'], 0, 50, 'utf-8'); ?></div>
+                      <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100, 'utf-8') . ' ...'; ?></div>
                     </div>
                   </article>
 
@@ -69,7 +69,47 @@ require "includes/config.php";
               </div>
             </div>
 
-            
+            <div class="block">
+              <a href="/articles.php?categorie=6">Все записи</a>
+              <h3>Безопасность [Новейшее]</h3>
+              <div class="block__content">
+                <div class="articles articles__horizontal">
+
+                  <?php
+                    $articles = mysqli_query($connection, "SELECT * FROM `articles` WHERE `categorie_id` = 6 ORDER BY `id` DESC LIMIT 10");
+                  ?>
+
+                  <?php
+                    while ($art = mysqli_fetch_assoc($articles)) {
+                    ?>
+
+                    <article class="article">
+                    <div class="article__image" style="background-image: url(/static/images/<?php echo $art['image']; ?>);"></div>
+                    <div class="article__info">
+                      <a href="/article.php?id=<?php echo $art['id']; ?>"><?php echo $art['title']; ?></a>
+                      <div class="article__info__meta">
+                        <?php
+                          $art_cat = false;
+                          foreach ($categories as $cat) {
+                            if ($cat['id'] == $art['categorie_id']) {
+                              $art_cat = $cat;
+                              break;
+                            }
+                          }
+                        ?>
+                        <small>Категория: <a href="/articles.php?categorie=<?php echo $art_cat['id'] ?>"><?php echo $art_cat['title']; ?></a></small>
+                      </div>
+                      <div class="article__info__preview"><?php echo mb_substr(strip_tags($art['text']), 0, 100, 'utf-8') . ' ...'; ?></div>
+                    </div>
+                  </article>
+
+                    <?php
+                    }
+                  ?>
+                  
+                </div>
+              </div>
+            </div>
 
             <div class="block">
               <a href="#">Все записи</a>
